e5f90bf3f6bd92541f379643f6fdb21b6e70c331
AlenaNosova <Teplykh91@gmail.com>
2022-11-19 13:38:35 +0700

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,16 @@
+let timer;
+let x = 59;
+
+function countDown() {
+    document.getElementById('timer').innerHTML = x;
+    x--;
+
+    if (x < 0) {
+        clearTimeout (timer);
+        alert("Вы победили в конкурсе!")
+    } else {
+        timer = setTimeout(countDown, 1000);
+    }
+}
+
+countDown();
