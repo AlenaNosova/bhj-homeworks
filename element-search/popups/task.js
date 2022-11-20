5c5b380fa3354f5a5f779e330582b9c7a8714b2a
AlenaNosova <Teplykh91@gmail.com>
2022-11-20 16:16:48 +0700

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+let modalMain = document.getElementById('modal_main');
+let modalClose = document.getElementsByClassName('modal__close');
+let modalShowSuccess = document.getElementsByClassName('show_success');
+let modalSuccess = document.getElementById('modal_success');
+
+
+modalMain. addEventListener('load', function() {
+    modalMain.classList.add('modal_active');
+})
+
+modalClose.onclick = function(){
+
+}
+
+modalShowSuccess.onclick = function() {
+    modalSuccess.show();
+}
