7bbc913782be79507d6d2c2d37592c61f2f95ec4
AlenaNosova <Teplykh91@gmail.com>
2022-12-20 18:30:16 +0700

    second commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,24 @@
-let menuItem = document.getElementsByClassName('menu__item');
-let links = document.getElementsByClassName('menu__link');
-let menu = document.getElementsByClassName('menu');
-let arr = Array.from(links);
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(el => {
+    el.addEventListener('click', (e) => {
+        let currentBtn = e.currentTarget; //текущий элемент
+        let dropdown = currentBtn.closest('.menu_sub').querySelector('.menu__link'); //находим выпадающее меню
+
+        dropdown.classList.toggle('menu_active'); //показать выпадющее меню
 
 
-for (let i =0; i > arr.length; i++) {
-    arr[i].onclick = function () {
-        if (menuItem.querySelectorAll('.menu')) {
-            menu.addClasslist('menu_active');
-            return false;
-        }
-    }
-}
+        //запретить переход по ссылке в выпадающем меню
+        dropdown.addEventListener('click', (event) => {
+            event.preventDefault()
+        })
+        
+
+        //одновременно не должно быть открыто более одного меню
+        currentBtn.forEach(el => {
+            if (el !== currentBtn) {
+                el.classList.remove('menu_active');
+            }
+        });
+    }) 
+})
