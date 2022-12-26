6f1a3ed6eeef9cf3fc7426d271ed602abcb1b2fe
AlenaNosova <Teplykh91@gmail.com>
2022-12-26 17:36:04 +0700

    fourth commit

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,30 +1,31 @@
 let dead = document.getElementById('dead');
 let lost = document.getElementById('lost');
+let hole = document.querySelectorAll('.hole')
 let deadMole = 0;
 let lostMole = 0;
 
-for(let i = 1; i <= 9; i++) {
-    let holeMole = function getHole(index) {
-        let click = document.getElementById(`hole${index}`);
-        return click;
-    }
-
-    document.getElementsByClassName('hole').onclick = function() {
-        if (holeMole[i].classList.containes('hole_has-mole')) {
+hole.forEach(function(el) {
+    el.addEventListener('click', function(){
+        if (el.className.includes('hole_has-mole')) {
             deadMole++; 
             dead.textContent = deadMole; 
         } else {
             lostMole++;
             lost.textContent = lostMole;
         }
-        if (lost.textContent == 5) {
-            alert ('Поражение! Попробуйте ещё раз!');
-            dead.textContent = 0;
-            lost.textContent = 0;
-        } else if (dead.textContent == 10) {
-            alert ('Победа!')
-            dead.textContent = 0;
-            lost.textContent = 0;
-        }
+
+    if (lost.textContent == 5) {
+        alert ('Поражение! Попробуйте ещё раз!');
+        dead.textContent = 0;
+        lost.textContent = 0;
+        deadMole = 0;
+        lostMole = 0;
+    } else if (dead.textContent == 10) {
+        alert ('Победа!')
+        dead.textContent = 0;
+        lost.textContent = 0;
+        deadMole = 0;
+        lostMole = 0;
     }
-} 
+    })
+}) 
