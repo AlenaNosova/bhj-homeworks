be5deb40e2768cba4b3922122b7a4bedf1b62b57
AlenaNosova <Teplykh91@gmail.com>
2022-12-26 20:31:19 +0700

    third commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,32 +1,31 @@
-let arrowPrev = document.querySelector('.slider__arrows');
-let arrowNext = document.getElementsByClassName('slider__arrow_next');
-let sliderItems = Array.from(document.querySelector('.slider__item'));
+const arrowPrev = document.querySelector('.slider__arrow_prev');
+const arrowNext = document.querySelector('.slider__arrow_next');
+const sliderItems = document.querySelectorAll('.slider__item');
 let current = 0;
 
-function slider() {
-    for(let i = 0; i < sliderItems.length; i++) {
-        sliderItems[i].classList.add('slider__item_active'); 
-        sliderItems[current].classList.remove('slider__item_active');       
+function slider(){
+    for (let i = 0; i < sliderItems.length; i++){
+        sliderItems[i].classList.remove('slider__item_active');
     }
+    sliderItems[current].classList.add('slider__item_active');
 }
-slider()
 
-arrowPrev.onclick = function(){
-    if(current - 1 == -1){
+arrowPrev.onclick = function() {
+    if (current + 1 == sliderItems.length) {
+        current = 0;
+    } else {
+        current++;
+    }
+    slider()
+};
+arrowNext.onclick = function() {
+    if (current - 1 == -1) {
         current = sliderItems.length - 1;
-    } else{
+    } else {
         current--;
     }
     slider()
 };
 
-arrowNext.onclick = function(){
-    if(current - 1 == sliderItems.length){
-        current = 0;
-    } else{
-        current++;
-    }
-    slider()
-};
  
 
