bab6b9a5802d60dae7b5ac7f19a4e074733948cf
AlenaNosova <Teplykh91@gmail.com>
2022-12-28 14:35:23 +0700

    third commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,21 +1,24 @@
-let tabs = document.querySelectorAll('.tab');
-let tabContent = document.querySelectorAll('.tab__content');
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.tab__content');
 
 for(let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', (event) => {
+    tabs[i].addEventListener('click',(event) => {
         
-        let currentTab = event.target.closest('.tab__navigation');
-        for(let t = 0; t < currentTab.length; t++){
-            currentTab[t].classlist.remove('tab_active');
+        let tabsCurrent = event.target.parentElement.children;
+        for(let t = 0; t < tabsCurrent.length; t++) {
+            tabsCurrent[t].classList.remove('tab_active');
         }
+        
         event.target.classList.add('tab_active');
 
 
-        let currentContent = event.target.closest('.tab__contents');
-        for(let c = 0; c < currentContent.length; t++){
-            currentContent[c].classlist.remove('tab__content_active');
+        let contentsCurrent = event.target.parentElement.nextElementSibling.children;
+        for(let c = 0; c < contentsCurrent.length; c++) {
+            contentsCurrent[c].classList.remove('tab__content_active');
         }
-        tabContent[i].classList.add('tab__content_active');
+        
+        contents[i].classList.add('tab__content_active');
 
     })
 }
+
