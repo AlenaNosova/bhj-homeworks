95b4ac0df98a6eb86260c4bbcdba77dd06da309c
AlenaNosova <Teplykh91@gmail.com>
2022-12-30 15:59:25 +0700

    second commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,11 +1,22 @@
-function ads () {
-    const rotator = this.closest('.rotator');
-    const rotatorCase = rotator.querySelector('span');
+const rotator = document.querySelectorAll('.rotator__case');
+let current = 0;
 
-    while(rotatorCase){
-        setInterval(() => {
-            const x = rotatorCase.classList.toggle('rotator__case_active');
-            rotatorCase.textContent = x;
-        }, 1000)
+function ads (){
+    for (let i = 0; i < rotator.length; i++) {
+                rotator[i].classList.remove('rotator__case_active');
+        }
+        rotator[current].classList.add('rotator__case_active');
+    };
+
+setInterval(() => {
+    if (current + 1 == rotator.length) {
+        current = 0;
+    } else {
+        current ++;
     }
-} 
+
+    ads();
+}, 1000)
+
+    
+ 
