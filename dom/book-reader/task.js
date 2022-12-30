4c6edcb32947bee56591c0694332567959410a43
AlenaNosova <Teplykh91@gmail.com>
2022-12-30 17:55:07 +0700

    second commit book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,20 +1,25 @@
-function bookReader(){
-    const book = this.closest('.book');
-    const fontSize = book.querySelector('a');
+const fonts = document.querySelectorAll('.font-size');
+const book = document.getElementById('book');
 
-    fontSize.addEventListener('click', function (){
-        const x = fontSize.classList.toggle('font-size_active');
-
-        if (x.classList.contains('font-size_small')) {
-            fontSize.classList.add('book_fs-small')
+for(let i = 0; i < fonts.length; i++) {
+    fonts[i].addEventListener('click',(event) => {
+        event.preventDefault();
+        let fontCurrent = event.target.parentElement.children;
+        for(let t = 0; t < fontCurrent.length; t++) {
+            fontCurrent[t].classList.remove('font-size_active');
         }
 
-        if (x.classList.contains('font-size_big')){
-            fontSize.classList.add('book_fs-big');
-        }
-
+        if(fonts[i].classList.contains('font-size_small')) {
+            event.target.classList.add('font-size_active');
+            book.classList.add('book_fs-small');
+        } else if(fonts[i].classList.contains('font-size_big')) {
+            book.classList.remove('book_fs-small');
+            event.target.classList.add('font-size_active');
+            book.classList.add('book_fs-big');
+        } else {
+            book.classList.remove('book_fs-small');
+            book.classList.remove('book_fs-big');
+            event.target.classList.add('font-size_active');
+        }      
     })
-
 }
-
-
